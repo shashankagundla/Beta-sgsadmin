81aa959925539056fd15605082cfa016d8d1f5c9
Andrew Beyer <andrew.beyer@sgstowers.com>
2016-08-27 00:51:20 -0400

    Updated Templater Class

--- includes/class/template.class.php
+++ includes/class/template.class.php
@@ -3,15 +3,13 @@
 class Template {
 
 function header($title) {
-    return '
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>'.$title.'</title>
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-    <script src="//code.jquery.com/jquery-2.1.0.min.js"></script>
-</head> 
-';
+    return '<html>
+            <head>
+                <meta charset="utf-8">
+                <title>'.$title.'</title>
+                <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+                <script src="//code.jquery.com/jquery-2.1.0.min.js"></script>
+            </head>';
 }
 
     function footer(){
