4e0b2278e2948bf28005a4c20545d0f981f9fe0a
Andrew Beyer <andrew.beyer@sgstowers.com>
2016-08-27 00:27:22 -0400

    includes reorg

--- account/index.php
+++ account/index.php
@@ -1,56 +1,59 @@
 <?php
 require_once("../includes/init.php");
 ?>
-
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Account Information</title>
-<style type="text/css">
-h1
-{
-font-family:Arial, Helvetica, sans-serif;
-color:#999999;
-}
-.wrapper{width:600px; margin-left:auto;margin-right:auto;}
-.welcome_txt{
-	margin: 20px;
-	background-color: #EBEBEB;
-	padding: 10px;
-	border: #D6D6D6 solid 1px;
-	-moz-border-radius:5px;
-	-webkit-border-radius:5px;
-	border-radius:5px;
-}
-.google_box{
-	margin: 20px;
-	background-color: #FFF0DD;
-	padding: 10px;
-	border: #F7CFCF solid 1px;
-	-moz-border-radius:5px;
-	-webkit-border-radius:5px;
-	border-radius:5px;
-}
-.google_box .image{ text-align:center;}
-</style>
+    <meta charset="utf-8">
+    <title>Bootswatch API Demo</title>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
 </head>
 <body>
-<div class="wrapper">
-    <h1>Google Profile Details </h1>
-    <?php
-    echo '<div class="welcome_txt">Welcome <b>'.$_SESSION['google_data']['given_name'].'</b></div>';
-    echo '<div class="google_box">';
-    echo '<p class="image"><img src="'.$_SESSION['google_data']['picture'].'" alt="" width="300" height="220"/></p>';
-    echo '<p><b>Google ID : </b>' . $_SESSION['google_data']['id'].'</p>';
-    echo '<p><b>Name : </b>' . $_SESSION['google_data']['name'].'</p>';
-    echo '<p><b>Email : </b>' . $_SESSION['google_data']['email'].'</p>';
-    echo '<p><b>Gender : </b>' . $_SESSION['google_data']['gender'].'</p>';
-    echo '<p><b>Locale : </b>' . $_SESSION['google_data']['locale'].'</p>';
-    echo '<p><b>Google+ Link : </b>' . $_SESSION['google_data']['link'].'</p>';
-    echo '<p><b>You are login with : </b>Google</p>';
-    echo '<p><b>Logout from <a href="logout/?logout">Google</a></b></p>';
-    echo '</div>';
-    ?>
+<h1>Bootstrap</h1>
+<div class="alert alert-info">
+    <div class="wrapper">
+        <h1>Google Profile Details </h1>
+        <?php
+        echo '<div class="welcome_txt">Welcome <b>'.$_SESSION['google_data']['given_name'].'</b></div>';
+        echo '<div class="google_box">';
+        echo '<p class="image"><img src="'.$_SESSION['google_data']['picture'].'" alt="" width="300" height="220"/></p>';
+        echo '<p><b>Google ID : </b>' . $_SESSION['google_data']['id'].'</p>';
+        echo '<p><b>Name : </b>' . $_SESSION['google_data']['name'].'</p>';
+        echo '<p><b>Email : </b>' . $_SESSION['google_data']['email'].'</p>';
+        echo '<p><b>Gender : </b>' . $_SESSION['google_data']['gender'].'</p>';
+        echo '<p><b>Locale : </b>' . $_SESSION['google_data']['locale'].'</p>';
+        echo '<p><b>Google+ Link : </b>' . $_SESSION['google_data']['link'].'</p>';
+        echo '<p><b>You are login with : </b>Google</p>';
+        echo '<p><b>Logout from <a href="logout/?logout">Google</a></b></p>';
+        echo '</div>';
+        ?>
+    </div>
 </div>
+<select></select>
 </body>
+<script>
+    $.getJSON("https://bootswatch.com/api/3.json", function (data) {
+        var themes = data.themes;
+        var select = $("select");
+        select.show();
+        $(".alert").toggleClass("alert-info alert-success");
+        $(".alert h4").text("Success!");
+
+        themes.forEach(function(value, index){
+            select.append($("<option />")
+                .val(index)
+                .text(value.name));
+        });
+
+        select.change(function(){
+            var theme = themes[$(this).val()];
+            $("link").attr("href", theme.css);
+            $("h1").text(theme.name);
+        }).change();
+
+    }, "json").fail(function(){
+        $(".alert").toggleClass("alert-info alert-danger");
+        $(".alert h4").text("Failure!");
+    });
+</script>
 </html>
--- includes/init.php
+++ includes/init.php
@@ -1,6 +1,10 @@
 <?php
 //required PHP files
 require_once('class/user.class.php');
+require_once('class/template.class.php');
+
+//init template
+$template = new Template;
 
 //check if user is authenticated
 $user = new Users;
