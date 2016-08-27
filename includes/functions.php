fd7180c26da73fec0c3d87c5d88e442faf1f9260
Andrew Beyer <andrew.beyer@sgstowers.com>
2016-08-26 22:33:47 -0400

    User & mysql class updates

--- includes/functions.php
+++ includes/functions.php
@@ -1,38 +1,56 @@
 <?php
-class db_wrapper{
-    public function __construct(){
-    //database configuration
-                $dbServer = 'sgsdb.cxdcv7nlogaf.us-east-1.rds.amazonaws.com'; //Define database server host
-                $dbUsername = 'sgsadmin'; //Define database username
-                $dbPassword = '8UWGozkMw3mpak'; //Define database password
-                $dbName = 'sgsadmin_beta'; //Define database name
 
-                //connect databse
-                $con = mysqli_connect($dbServer,$dbUsername,$dbPassword,$dbName);
-                if(mysqli_connect_errno()){
-                        die("Failed to connect with MySQL: ".mysqli_connect_error());
-                }else{
-                        $this->connect = $con;
-                }
-    }; #connect to db
-    public function escape($string);
-    public function query($sql);
-    public function get_results();
-}
+include("mysql.class.php");
+
+if ($db->Error()) $db->Kill();
 
 class Users {
 	public $tableName = 'users';
-	function checkUser($oauth_provider,$oauth_uid,$fname,$lname,$email,$gender,$locale,$link,$picture){
-		$prevQuery = mysqli_query($this->connect,"SELECT * FROM $this->tableName WHERE oauth_provider = '".$oauth_provider."' AND oauth_uid = '".$oauth_uid."'") or die(mysqli_error($this->connect));
-		if(mysqli_num_rows($prevQuery) > 0){
-			$update = mysqli_query($this->connect,"UPDATE $this->tableName SET oauth_provider = '".$oauth_provider."', oauth_uid = '".$oauth_uid."', fname = '".$fname."', lname = '".$lname."', email = '".$email."', gender = '".$gender."', locale = '".$locale."', picture = '".$picture."', gpluslink = '".$link."', modified = '".date("Y-m-d H:i:s")."' WHERE oauth_provider = '".$oauth_provider."' AND oauth_uid = '".$oauth_uid."'") or die(mysqli_error($this->connect));
+
+    function checkUser($oauth_provider,$oauth_uid,$fname,$lname,$email,$gender,$locale,$link,$picture){
+        // Create an array that holds the update information
+        // $arrayVariable["column name"] = formatted SQL value
+        $oauthData["oauth_provider"] = MySQL::SQLValue($oauth_provider);
+        $oauthData["oauth_uid"] = MySQL::SQLValue($oauth_uid);
+        $oauthData["fname"] = MySQL::SQLValue($fname);
+        $oauthData["lname"] = MySQL::SQLValue($lname);
+        $oauthData["email"] = MySQL::SQLValue($email);
+        $oauthData["gender"] = MySQL::SQLValue($gender);
+        $oauthData["locale"] = MySQL::SQLValue($locale);
+        $oauthData["picture"] = MySQL::SQLValue($picture);
+        $oauthData["gpluslink"] = MySQL::SQLValue($link);
+        $oauthData["modified"] = MySQL::SQLValue(date("Y-m-d H:i:s"));
+
+        //Check if User table exists
+        if (! $db->Query("SELECT * FROM $this->tableName")) $db->Kill();
+
+        //Check for user in db
+        $prevQuery = $db->Query("SELECT * FROM $this->tableName WHERE oauth_provider = '".$oauth_provider."' AND oauth_uid = '".$oauth_uid."'");
+
+        //If users is in db then update
+        if($db->RowCount() > 0){
+
+            // Create a filter array the determines which record(s) to process
+            // (you can specify more than one column if needed)
+            $where["oauth_provider"] = MySQL::SQLValue($oauth_provider);
+            $where["oauth_uid"] = MySQL::SQLValue($oauth_uid);
+
+            // Execute the update
+            $result = $db->UpdateRows($this->tableName, $oauthData, $where);
+
 		}else{
-			$insert = mysqli_query($this->connect,"INSERT INTO $this->tableName SET oauth_provider = '".$oauth_provider."', oauth_uid = '".$oauth_uid."', fname = '".$fname."', lname = '".$lname."', email = '".$email."', gender = '".$gender."', locale = '".$locale."', picture = '".$picture."', gpluslink = '".$link."', created = '".date("Y-m-d H:i:s")."', modified = '".date("Y-m-d H:i:s")."'") or die(mysqli_error($this->connect));
+
+            // Insert New User to DB
+            $result = $db->InsertRow($this->tableName, $oauthData);
+
 		}
-		
-		$query = mysqli_query($this->connect,"SELECT * FROM $this->tableName WHERE oauth_provider = '".$oauth_provider."' AND oauth_uid = '".$oauth_uid."'") or die(mysqli_error($this->connect));
-		$result = mysqli_fetch_array($query);
-		return $result;
+        // If we have an error display & kill
+        if (! $result) { $db->Kill(); }
+
+        //Finally requery db for user data
+        $authUser = $db->Query("SELECT * FROM $this->tableName WHERE oauth_provider = '".$oauth_provider."' AND oauth_uid = '".$oauth_uid."'");
+
+        return $authUser;
 	}
 }
 ?>
