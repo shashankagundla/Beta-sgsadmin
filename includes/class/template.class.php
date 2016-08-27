1ff8d6ed7765d5f980f022d5fd8ef0853773856d
Andrew Beyer <andrew.beyer@sgstowers.com>
2016-08-27 01:38:40 -0400

    Added Theme Changer

--- includes/class/template.class.php
+++ includes/class/template.class.php
@@ -7,7 +7,6 @@ class Template {
 <head>
     <meta charset="utf-8">
     <title>'.$title.'</title>
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
     <script src="//code.jquery.com/jquery-2.1.0.min.js"></script>
     <script>$.getJSON("https://bootswatch.com/api/3.json",function(a){var b=a.themes,d=($("select"),b['.$theme.']);$("link").attr("href",d.css)},"json").fail(function(){$(".alert").toggleClass("alert-info alert-danger"),$(".alert h4").text("Failure!")});</script>
 </head>';
