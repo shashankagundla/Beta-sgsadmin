ef4bf6ac11c05a93aa7102b299eaff156178ea5a
Andrew Beyer <andrew.beyer@sgstowers.com>
2016-08-27 00:57:49 -0400

    Updated Templater Class

--- account/index.php
+++ account/index.php
@@ -25,30 +25,10 @@ echo $template->header('User Account');
     </div>
 </div>
 <select></select>
+
+<?php
+echo $template->footer();
+?>
+
 </body>
-<script>
-    $.getJSON("https://bootswatch.com/api/3.json", function (data) {
-        var themes = data.themes;
-        var select = $("select");
-        select.show();
-        $(".alert").toggleClass("alert-info alert-success");
-        $(".alert h4").text("Success!");
 
-        themes.forEach(function(value, index){
-            select.append($("<option />")
-                .val(index)
-                .text(value.name));
-        });
-
-        select.change(function(){
-            var theme = themes[$(this).val()];
-            $("link").attr("href", theme.css);
-            $("h1").text(theme.name);
-        }).change();
-
-    }, "json").fail(function(){
-        $(".alert").toggleClass("alert-info alert-danger");
-        $(".alert h4").text("Failure!");
-    });
-</script>
-</html>
--- includes/class/template.class.php
+++ includes/class/template.class.php
@@ -2,18 +2,22 @@
 
 class Template {
 
-function header($title) {
-    return '<html>
-            <head>
-                <meta charset="utf-8">
-                <title>'.$title.'</title>
-                <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-                <script src="//code.jquery.com/jquery-2.1.0.min.js"></script>
-            </head>';
-}
-
-    function footer(){
-
+    function header($title) {
+        return '<html>
+<head>
+    <meta charset="utf-8">
+    <title>'.$title.'</title>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <script src="//code.jquery.com/jquery-2.1.0.min.js"></script>
+</head>';
     }
+
+    function footer() {
+        return '<script src="/assets/js/bs.js"></script>
+</html>';
+    }
+
+
+
 }
 ?>
