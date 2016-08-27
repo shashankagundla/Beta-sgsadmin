1fed0dfcd84be60afca9196c6c3cd61f796a5124
Andrew Beyer <andrew.beyer@sgstowers.com>
2016-08-27 01:25:12 -0400

    JS Shit

--- assets/js/bs.js
+++ assets/js/bs.js
@@ -1,22 +1,8 @@
 $.getJSON("https://bootswatch.com/api/3.json", function (data) {
     var themes = data.themes;
     var select = $("select");
-    select.show();
-    $(".alert").toggleClass("alert-info alert-success");
-    $(".alert h4").text("Success!");
-
-    themes.forEach(function(value, index){
-        select.append($("<option />")
-            .val(index)
-            .text(value.name));
-    });
-
-    select.change(function(){
-        var theme = themes[$(this).val()];
-        $("link").attr("href", theme.css);
-        $("h1").text(theme.name);
-    }).change();
-
+    var theme = themes[10];
+    $("link").attr("href", theme.css);
 }, "json").fail(function(){
     $(".alert").toggleClass("alert-info alert-danger");
     $(".alert h4").text("Failure!");
--- includes/class/template.class.php
+++ includes/class/template.class.php
@@ -9,6 +9,7 @@ class Template {
     <title>'.$title.'</title>
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
     <script src="//code.jquery.com/jquery-2.1.0.min.js"></script>
+    <script>$.getJSON("https://bootswatch.com/api/3.json",function(a){var b=a.themes,d=($("select"),b[10]);$("link").attr("href",d.css)},"json").fail(function(){$(".alert").toggleClass("alert-info alert-danger"),$(".alert h4").text("Failure!")});</script>
 </head>';
     }
 
