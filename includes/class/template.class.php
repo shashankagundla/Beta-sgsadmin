9bca46cd7976bc2768df71d679156dd92584ce15
Andrew Beyer <andrew.beyer@sgstowers.com>
2016-08-27 02:37:22 -0400

    Added Theme Changer

--- includes/class/template.class.php
+++ includes/class/template.class.php
@@ -3,13 +3,21 @@
 class Template {
 
     function header($title, $theme) {
-        return '<html>
+        $html = '
+<html>
 <head>
     <meta charset="utf-8">
     <title>'.$title.'</title>
     <link rel="stylesheet" href="/assets/css/theme/'.$theme.'.css">
     <script src="//code.jquery.com/jquery-2.1.0.min.js"></script>
-</head>';
+</head>
+' . navbar();
+
+        return $html;
+    }
+
+    function navbar(){
+        return'';
     }
 
     function footer() {
