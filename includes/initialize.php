a25db1d456542f79bdc33804fa32f123d7a97374
Andrew Beyer <andrew.beyer@sgstowers.com>
2016-08-26 23:23:55 -0400

    Added init script

--- includes/initialize.php
+++ includes/initialize.php
@@ -1,19 +1,9 @@
 <?
-
-// define paths
-define("APP_ROOT", "/");
-define("INC_PATH", APP_ROOT . "/includes");
-define("PUBLIC_PATH", APP_ROOT . "/");
-
-// define URL
-define("URL", "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
-define("ESCAPED_URL", htmlspecialchars(URL, ENT_QUOTES, 'UTF-8'));
-
 // start the PHP session
 session_start();
 
 // require all necessary php files for application
-require_once(INC_PATH . "/functions.php");
+require_once('functions.php');
 
 // to show session variables for testing when on local, put ?ss=true at the end of the query string in the browser
 $ss = (isset($_GET["ss"])) ? $_GET["ss"] : '';
