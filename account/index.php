6cc124a6cc90f700bbe07165e3656c32d2368def
Andrew Beyer <andrew.beyer@sgstowers.com>
2016-08-27 00:28:08 -0400

    includes reorg

--- account/index.php
+++ account/index.php
@@ -6,7 +6,7 @@ require_once("../includes/init.php");
     <meta charset="utf-8">
     <title>Bootswatch API Demo</title>
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-    <script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+    <script src="//code.jquery.com/jquery-2.1.0.min.js"></script>
 </head>
 <body>
 <h1>Bootstrap</h1>
