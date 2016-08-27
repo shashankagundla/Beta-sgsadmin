a57ed7ed9a27487d42c725126088fa00f315fa3e
Andrew Beyer <andrew.beyer@sgstowers.com>
2016-08-27 02:30:55 -0400

    Added Theme Changer

--- includes/class/template.class.php
+++ includes/class/template.class.php
@@ -7,7 +7,7 @@ class Template {
 <head>
     <meta charset="utf-8">
     <title>'.$title.'</title>
-    <link rel="stylesheet" href="/assets/css/theme/'.$theme.'">
+    <link rel="stylesheet" href="/assets/css/theme/'.$theme.'.css">
     <script src="//code.jquery.com/jquery-2.1.0.min.js"></script>
 </head>';
     }
