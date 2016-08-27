27374dbd5aa0d1e9da2906b7a90eccb5c0370ad9
Andrew Beyer <andrew.beyer@sgstowers.com>
2016-08-27 02:29:17 -0400

    Added Theme Changer

--- account/index.php
+++ account/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once("../includes/init.php");
-echo $template->header('User Account', 10);
+echo $template->header('User Account', 15);
 ?>
 
 <body>
--- includes/class/template.class.php
+++ includes/class/template.class.php
@@ -7,7 +7,7 @@ class Template {
 <head>
     <meta charset="utf-8">
     <title>'.$title.'</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/latest/cerulean/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/theme/'.$theme.'">
     <script src="//code.jquery.com/jquery-2.1.0.min.js"></script>
 </head>';
     }
