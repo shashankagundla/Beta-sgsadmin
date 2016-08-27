f2917996b3a3b6e9ad00aee5236c35466995e862
Andrew Beyer <andrew.beyer@sgstowers.com>
2016-08-26 23:28:19 -0400

    Added init script

--- includes/initialize.php
+++ includes/initialize.php
@@ -5,14 +5,6 @@ session_start();
 // require all necessary php files for application
 require_once('functions.php');
 
-// to show session variables for testing when on local, put ?ss=true at the end of the query string in the browser
-$ss = (isset($_GET["ss"])) ? $_GET["ss"] : '';
-if($ss != "") {
-    echo '<pre>';
-    var_dump($_SESSION);
-    echo '</pre>';
-}
-
 // set current timezone
 date_default_timezone_set('America/Detroit');
 
