7cf4f55f5b7c0f2206d3837f539770b34f8fef38
Andrew Beyer <andrew.beyer@sgstowers.com>
2016-09-21 20:44:25 -0400

    Dev

--- tests/vfm/vfm-admin/config.php
+++ tests/vfm/vfm-admin/config.php
@@ -85,7 +85,7 @@
   'smtp_auth' => false,
   'smtp_enable' => false,
   'smtp_server' => '',
-  'starting_dir' => '/data/',
+  'starting_dir' => './uploads/',
   'thumbnails' => true,
   'thumbnails_height' => 800,
   'thumbnails_width' => 760,
